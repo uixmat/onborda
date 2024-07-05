e4fcb2d2673a3611f0375d2196c18c646c15d710
Jose Alfaro <josealfacr@gmail.com>
2024-07-04 19:47:45 -0600

    ➕ adding side options

--- src/Onborda.tsx
+++ src/Onborda.tsx
@@ -214,6 +214,52 @@ const Onborda: React.FC<OnbordaProps> = ({
           top: "50%",
           marginLeft: "25px",
         };
+      case "top-left":
+        return {
+          bottom: "100%",
+          marginBottom: "25px",
+        };
+      case "top-right":
+        return {
+          right: 0,
+          bottom: "100%",
+          marginBottom: "25px",
+        };
+      case "bottom-left":
+        return {
+          top: "100%",
+          marginTop: "25px",
+        };
+      case "bottom-right":
+        return {
+          right: 0,
+          top: "100%",
+          marginTop: "25px",
+        };
+      case "right-bottom":
+        return {
+          left: "100%",
+          bottom: 0,
+          marginLeft: "25px",
+        };
+      case "right-top":
+        return {
+          left: "100%",
+          top: 0,
+          marginLeft: "25px",
+        };
+      case "left-bottom":
+        return {
+          right: "100%",
+          bottom: 0,
+          marginRight: "25px",
+        };
+      case "left-top":
+        return {
+          right: "100%",
+          top: 0,
+          marginRight: "25px",
+        };
       default:
         return {}; // Default case if no side is specified
     }
@@ -246,6 +292,54 @@ const Onborda: React.FC<OnbordaProps> = ({
           transform: `translate(0, -50%) rotate(0deg)`,
           top: "50%",
           right: "-23px",
+        };
+      case "top-left":
+        return {
+          transform: `rotate(90deg)`,
+          left: "10px",
+          bottom: "-23px",
+        };
+      case "top-right":
+        return {
+          transform: `rotate(90deg)`,
+          right: "10px",
+          bottom: "-23px",
+        };
+      case "bottom-left":
+        return {
+          transform: `rotate(270deg)`,
+          left: "10px",
+          top: "-23px",
+        };
+      case "bottom-right":
+        return {
+          transform: `rotate(270deg)`,
+          right: "10px",
+          top: "-23px",
+        };
+      case "right-bottom":
+        return {
+          transform: `rotate(180deg)`,
+          left: "-23px",
+          bottom: "10px",
+        };
+      case "right-top":
+        return {
+          transform: `rotate(180deg)`,
+          left: "-23px",
+          top: "10px",
+        };
+      case "left-bottom":
+        return {
+          transform: `rotate(0deg)`,
+          right: "-23px",
+          bottom: "10px",
+        };
+      case "left-top":
+        return {
+          transform: `rotate(0deg)`,
+          right: "-23px",
+          top: "10px",
         };
       default:
         return {}; // Default case if no side is specified
